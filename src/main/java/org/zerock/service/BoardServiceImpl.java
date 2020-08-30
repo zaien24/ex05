6a84ba85edf3492453e1ed42629e0eb1d1d67501
zaien24 <zaien24@gmail.com>
2020-08-30 17:41:35 +0900

    비즈니스계층 구현과 테스트 웹 계층 구현과 테스트

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,69 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	// spring 4.3 이상에서 자동 처리
+	/*
+	 * @Setter(onMethod_ = @Autowired) private BoardMapper mapper;
+	 */
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		
+		log.info("register ....." + board);
+		
+		mapper.insertSelectKey(board);
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		
+		log.info("get....." + bno);
+		
+		return mapper.read(bno);
+	}
+
+	
+
+	@Override
+	public List<BoardVO> getList() {
+		
+		log.info("getList .............");
+		
+		return mapper.getList();
+	}
+
+	
+	@Override
+	public boolean modify(BoardVO board) {
+		
+		log.info("modify........" + board);
+		
+		return mapper.update(board) == 1;
+	}
+	
+	@Override
+	public boolean remove(Long bno) {
+		
+		log.info("remove........" + bno);
+		
+		return mapper.delete(bno) == 1;
+	}
+	
+	
+
+}
