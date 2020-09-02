3bc8b6403858052440e4864984f9ca2469bbbe95
zaien24 <zaien24@gmail.com>
2020-09-02 13:04:14 +0900

    297~299 [페이징처리]  # BoardService 수정  # 페이징 테스트

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.mapper.BoardMapper;
 
 import lombok.AllArgsConstructor;
@@ -37,14 +38,20 @@ public class BoardServiceImpl implements BoardService {
 		return mapper.read(bno);
 	}
 
+	/*
+	 * @Override public List<BoardVO> getList() {
+	 * 
+	 * log.info("getList .............");
+	 * 
+	 * return mapper.getList(); }
+	 */
 	
-
 	@Override
-	public List<BoardVO> getList() {
+	public List<BoardVO> getList(Criteria cri) {
 		
-		log.info("getList .............");
+		log.info("get List with criteria: " + cri);
 		
-		return mapper.getList();
+		return mapper.getListWithPaging(cri);
 	}
 
 	
@@ -63,6 +70,8 @@ public class BoardServiceImpl implements BoardService {
 		
 		return mapper.delete(bno) == 1;
 	}
+
+	
 	
 	
 
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -38,6 +38,10 @@ public class BoardMapperTests {
 		
 		Criteria cri = new Criteria();
 		
+		// 10개씩 3페이지
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		
 		List<BoardVO> list = mapper.getListWithPaging(cri);
 		
 		list.forEach(board -> log.info(board));
