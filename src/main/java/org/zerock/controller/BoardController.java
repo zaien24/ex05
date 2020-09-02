7d9791c5170efa35e2f3b7d8f5d63e1cefbc0012
zaien24 <zaien24@gmail.com>
2020-09-02 13:52:11 +0900

    300 ~ 305 [페이징처리]  # list 메서드 수정  # 콘트롤러 테스트  # 서비스 테스트 [페이징 처리를 위한 클래스 설계]  # PageDTO 설계

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -21,12 +22,20 @@ public class BoardController {
 	
 	private BoardService service;
 	
+	/*
+	 * @GetMapping("/list") public void list(Model model) {
+	 * 
+	 * log.info("list");
+	 * 
+	 * model.addAttribute("list", service.getList()); }
+	 */
+	
 	@GetMapping("/list")
-	public void list(Model model) {
+	public void list(Criteria cri, Model model) {
 		
-		log.info("list");
+		log.info("list: " + cri);
 		
-		model.addAttribute("list", service.getList());
+		model.addAttribute("list", service.getList(cri));
 	}
 	
 	/*
--- src/test/java/org/zerock/controller/BoardControllerTests.java
+++ src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -37,10 +37,12 @@ public class BoardControllerTests {
 	public void testList() throws Exception {
 		
 		log.info(
-				mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
-				.andReturn()
-				.getModelAndView()
-				.getModelMap());		
+				mockMvc.perform(
+					MockMvcRequestBuilders
+						.get("/board/list")
+						.param("pageNum", "2")
+						.param("amount", "50"))
+				.andReturn().getModelAndView().getModelMap());		
 	}
 	
 	@Test
