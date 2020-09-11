4df509d37d688e8d2fa15c21927e979b48ebf31c
zaien24 <zaien24@gmail.com>
2020-09-11 18:40:41 +0900

    507~513 [파일 업로드 상세처리]  # 중복된 이름의 첨부파일 처리  # 중복 방지를 위한 UUID 적용  # 썸네일 이미지 생성    - 이미지 파일의 판단

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,6 +1,11 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -47,12 +52,32 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+			
+		return str.replace("-", File.separator);
+	}
+
+	
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		
 		log.info("update ajax post.........");
 		
 		String uploadFolder = "C:\\upload";
+		
+		// make folder =----------
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path: " + uploadPath);
+		
+		if (uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		// make yyyy/MM/dd folder
 		
 		for (MultipartFile multipartFile : uploadFile) {
 			
@@ -66,7 +91,12 @@ public class UploadController {
 			
 			log.info("only file name : " + uploadFileName );
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//File saveFile = new File(uploadFolder, uploadFileName);
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
@@ -75,7 +105,18 @@ public class UploadController {
 			}
 			
 		}
+	}
+	
+	private boolean checkImageType(File file) {
 		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	
 }
